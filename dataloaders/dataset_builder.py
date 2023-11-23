aa1d75e30250a0a281c7355f350116dcf7df5766
CarmelaCalabrese <90254664+CarmelaCalabrese@users.noreply.github.com>
2023-11-23 18:05:24 +0100

    Update dataset_builder with Ssv2

--- dataloaders/dataset_builder.py
+++ dataloaders/dataset_builder.py
@@ -3,12 +3,14 @@ from .coco_detection import CocoDetection
 from .nus_wide import NUSWIDE_ZSL
 from .pascal_voc import voc2007
 from .ava_frame import AVA_ZSL
+from .ssv2 import Ssv2
 
 MODEL_TABLE = {
     'coco': CocoDetection,
     'nus_wide_zsl': NUSWIDE_ZSL,
     'voc2007': voc2007,
-    'ava_frame': AVA_ZSL
+    'ava_frame': AVA_ZSL,
+    'ssv2': Ssv2
 }
 
 
